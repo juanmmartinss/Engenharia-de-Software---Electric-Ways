2082cd32f40b8dbe4dd26d0fa8631f9a3e45a393
juan <juanmmartinss@outlook.com>
2023-09-14 18:00:40 -0300

    Texto

--- My App/src/views/usuário/menuUsuário.py
+++ My App/src/views/usuário/menuUsuário.py
@@ -43,6 +43,12 @@ body = Container(
             alignment = alignment.center,
         ),
         
+        Container(
+            Text(
+                "Nome do Usuário",
+            ),
+        ),
+        
     ]
     ),
     
