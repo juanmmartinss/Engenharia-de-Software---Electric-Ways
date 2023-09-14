16a44007dff311bb176d5936b4620c7782fe015a
DaviSeiji <daviseiji@gmail.com>
2023-09-14 17:51:09 -0300

    Parte1 menu usuario

--- My App/src/views/usuário/menuUsuário.py
+++ My App/src/views/usuário/menuUsuário.py
@@ -22,129 +22,29 @@ botaoPadrao = ButtonStyle(
 
 body = Container(
     
-    Column([
+    Column(
+        [
         # MenuDetail(),
+
+        IconButton(
+            icon = icons.ARROW_BACK_IOS_NEW_ROUNDED,
+            icon_color = colors.WHITE,
+            icon_size = 40,
+            tooltip = "Voltar"
+        ),
+
         Container(
-            Image(
-                src = "https://i.ibb.co/VtsLycp/logo-without-bg.png",
-                scale=0.8,
-                height = 300,
-            ),
-            margin = margin.only(top = 30)
-        ), 
-
-        Column([
-
-            Container(
-                TextField(label = "Usuário", 
-                        bgcolor = colors.TRANSPARENT, 
-                        border_color = colors.WHITE, 
-                        border_width = 1,
-                        prefix_icon = "person_outline_rounded",
-                        border_radius = 20,
-                        width = 320,
-                        text_size= 9),
-                alignment = alignment.center
+            CircleAvatar(
+                foreground_image_url = "https://ibb.co/kMyPWrS",
+                content = Text("VOCÊ"),
+                radius = 90,
 
             ),
-            
-            Container(
-                TextField(label = "E-mail", 
-                        bgcolor = colors.TRANSPARENT, 
-                        border_color = colors.WHITE, 
-                        border_width = 1,
-                        prefix_icon = "mail_outline_rounded",
-                        border_radius = 20,
-                        width = 320,
-                        text_size= 9),
-                alignment = alignment.center
-
-            ),
-
-            Container(
-                TextField(label = "Senha",
-                        bgcolor = colors.TRANSPARENT,
-                        border_color = colors.WHITE, 
-                        border_width = 1,
-                        password = True,
-                        prefix_icon = "lock_outline_rounded",
-                        can_reveal_password = True,
-                        border_radius = 20,
-                        width = 320,
-                        text_size= 9),
-                alignment = alignment.center        
-            
-            ),
-
-            Container(
-                TextField(label = "Confirmar senha",
-                        bgcolor = colors.TRANSPARENT,
-                        border_color = colors.WHITE, 
-                        border_width = 1,
-                        password = True,
-                        prefix_icon = "lock_outline_rounded",
-                        can_reveal_password = True,
-                        border_radius = 20,
-                        width = 320,
-                        text_size= 9),
-                alignment = alignment.center        
-            
-            ),
-            Container(
-                Row(
-                    [
-                        
-                    Checkbox(),
-
-                    Text("Eu li e aceito os", size = 14, color = colors.WHITE),
-
-
-                    Container(
-                        TextButton(
-                            content = Container(
-                            Text(value = "Termos e Condições", size = 14, color = colors.WHITE)
-                            )
-                        ),
-                    ),
-
-
-                    ],
-                    spacing = 0, 
-                    alignment = MainAxisAlignment.START,
-                    
-                ),
-                margin = margin.only(left = 30, top = 10)
-            ),
+            alignment = alignment.center,
+        ),
         
-
-
-            Container(
-                ElevatedButton(
-                    content = Container(
-                        Text(value = "  Cadastrar  ", size = 30),
-                        ),
-                        style = botaoPadrao,
-                    ),
-
-                alignment = alignment.center,
-                margin = margin.only(top = 10)
-            ),
-            
-            Container(
-                TextButton(
-                    content = Container(
-                    Text(value = "Entrar", 
-                         size = 20, 
-                         style = ButtonStyle('underline'),
-                         color = colors.WHITE)
-                    )
-                ),
-                alignment = alignment.center
-            )
-
-
-        ])
-    ]),
+    ]
+    ),
     
     gradient= LinearGradient(
         begin=alignment.top_left,
