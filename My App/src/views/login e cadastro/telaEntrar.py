e60660ec0bb11713b30abdb8a8e5174bcdf32b71
DaviSeiji <daviseiji@gmail.com>
2023-09-19 20:18:48 -0300

    tentando ancorar

--- My App/src/views/login e cadastro/telaEntrar.py
+++ My App/src/views/login e cadastro/telaEntrar.py
@@ -17,14 +17,12 @@ botaoPadrao = ButtonStyle(
 body = Container(
     
     Column([
-        # MenuDetail(),
         Container(
             Image(
                 src = "https://i.ibb.co/VtsLycp/logo-without-bg.png",
                 scale=0.8,
                 height = 300,
             ),
-            margin = margin.only(top = 30)
         ), 
 
         Column([
@@ -81,7 +79,7 @@ body = Container(
                     ),
 
                 alignment = alignment.center,
-                margin = margin.only(top = 70)
+                padding = padding.only(top = 40),
             ),
             
             Container(
@@ -102,25 +100,108 @@ body = Container(
         colors=[colorBackground, colorBackground2],
     ), 
     
-    width=500,
-    height=800,
+    padding = 0,
 )
 
 
 def main(page: Page):
 
-    page.window_max_height = 800
-    page.window_width = 500
-    page. window_max_width = 500
-    page.window_height = 800
-    
     page.padding = 0
     
     page.add(
-        body
-    )
+       Container(
     
-    #butto
+            Column([
+                Container(
+                    Image(
+                        src = "https://i.ibb.co/VtsLycp/logo-without-bg.png",
+                        scale=0.8,
+                        height = 300,
+                    ),
+                ), 
+
+                Column([
+
+                    Container(
+                        TextField(label = "Usuário", 
+                                bgcolor = colors.TRANSPARENT, 
+                                border_color = colors.WHITE, 
+                                border_width = 1,
+                                prefix_icon = "person_outline_rounded",
+                                border_radius = 20,
+                                width = 320,
+                                text_size= 15,
+                                color = colors.WHITE,
+                                
+                            ),
+                        alignment = alignment.center,
+
+                    ),
+
+                    Container(
+                        TextField(label = "Senha",
+                                bgcolor = colors.TRANSPARENT,
+                                border_color = colors.WHITE, 
+                                border_width = 1,
+                                password = True,
+                                can_reveal_password = True,
+                                prefix_icon = "lock_outline_rounded",
+                                border_radius = 20,
+                                width = 320,
+                                text_size= 15,
+                                color = colors.WHITE
+                                ),
+                        alignment = alignment.center        
+                    
+                    ),
+
+                    Container(
+                        TextButton(
+                            content = Container(
+                            Text(value = "Esqueceu a senha?", size = 14, color = colors.WHITE)
+                            )
+                        ),
+                        margin = margin.only(left = 30)
+                    ),
+
+
+                    Container(
+                        ElevatedButton(
+                            content = Container(
+                                Text(value = "    Entrar     ", size = 30),
+                                ),
+                                style = botaoPadrao,
+                            ),
+
+                        alignment = alignment.center,
+                        padding = padding.only(top = 40),
+                    ),
+                    
+                    Container(
+                        TextButton(
+                            content = Container(
+                            Text(value = "Cadastre-se", size = 20, color = colors.WHITE)
+                            )
+                        ),
+                        alignment = alignment.center
+                    )
+
+                ])
+            ]),
+            
+            gradient= LinearGradient(
+                begin=alignment.top_left,
+                end=alignment.bottom_right,
+                colors=[colorBackground, colorBackground2],
+            ), 
+            
+            padding = 0,
+            width = page.width,
+            height = page.height,
+        )
+
+    )
+
     
 
         
