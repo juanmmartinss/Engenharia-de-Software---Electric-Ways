06f7555d18adf0454eeda5f1d1831c9de2e76510
savio37 <savio.am73@.com>
2023-09-22 21:58:50 -0300

    backend do cadastro

--- My App/src/app/cadastro.py
+++ My App/src/app/cadastro.py
@@ -1,8 +1,6 @@
 import sys, os
 sys.path.append(os.path.abspath(os.path.join('..', '..', 'database')))
 from database.SQLiteDB import *
-
-from src.database.SQLiteDB import *
 from flet import *
 
 
